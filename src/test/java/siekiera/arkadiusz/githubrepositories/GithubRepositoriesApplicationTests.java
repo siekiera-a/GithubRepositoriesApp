5a55eac7cb4f996acb4b362456e7342b0928b97c
Arkadiusz Siekiera <Arkadiusz.Siekiera@orange.com>
2021-12-28 22:14:13 +0100

    feat(tests): tests for get stars and repositories endpoints

--- src/test/java/siekiera/arkadiusz/githubrepositories/GithubRepositoriesApplicationTests.java
+++ src/test/java/siekiera/arkadiusz/githubrepositories/GithubRepositoriesApplicationTests.java
@@ -1,13 +1,64 @@
 package siekiera.arkadiusz.githubrepositories;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GithubRepositoriesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testGetRepositories_forUserAllegro_isNotEmptyList() throws Exception {
+        mockMvc.perform(get("/repos/allegro").accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpectAll(
+                status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$", Matchers.not(Matchers.empty()))
+            );
+    }
+
+    @Test
+    void testGetRepositories_forNonExistingUser_returns404() throws Exception {
+        mockMvc.perform(get("/repos/_"))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetUserStars_forUserAllegro_isGraterThanZero() throws Exception {
+        String username = "allegro";
+        mockMvc.perform(get(String.format("/repos/%s/stars", username)).accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpectAll(
+                status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$.username").value(username),
+                jsonPath("$.stars", Matchers.greaterThan(0))
+            );
+    }
+
+    @Test
+    void testGetUserStars_forNonExistingUser_returns404() throws Exception {
+        mockMvc.perform(get("/repos/_/stars"))
+            .andExpect(status().isNotFound());
+    }
 
 }
