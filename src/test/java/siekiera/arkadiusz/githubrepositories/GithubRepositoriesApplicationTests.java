b80c978cdba558824c524425268733df06d8627a
Arkadiusz Siekiera <Arkadiusz.Siekiera@orange.com>
2021-12-28 20:10:15 +0100

    initial commit

---
+++ src/test/java/siekiera/arkadiusz/githubrepositories/GithubRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package siekiera.arkadiusz.githubrepositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
